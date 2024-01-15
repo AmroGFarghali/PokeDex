79364022a9b9dabd55fe05c3d460d5eb237258c1
Amr Gamal <57310811+AmroGFarghali@users.noreply.github.com>
2024-01-15 17:08:05 +0300

    firstCommit

---
+++ pokePage.js
@@ -0,0 +1,3 @@
+const pokeImage = document.querySelector(".pokemon-sprite img");
+pokeImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg`;
+pokeImage.alt = name;
